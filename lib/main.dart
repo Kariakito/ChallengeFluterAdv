6e5a25308ebf618e3a0403ca8f84323a1cded2ec
Kariakito <pilarmaestre@gmail.com>
2024-01-30 12:35:01 +0000

    Adding Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:basics_avanzados/button.dart';
 
 void main() {
   runApp(
     const MaterialApp(
+      home: Scaffold(
+        body: ButtonApp(),
+      ),
 
     )
 
